60a2ac223b3ac1f451281a83fb2863ffe6d8ec24
Ahmed Fekry <ahmedfikry78@gmail.com>
2021-12-24 04:06:24 +0200

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Description](#description)
+* [Usage](#usage)
+* [Dependencies](#dependencies)
+
+## Description
+This is my first project in the Udacity nanodegree Professional Web Development Track. the aim of the project is to practice what i have learned in javascript so far. i have used functions like addEventListener and scrolling and event controlls
+
+
+## Usage
+
+Just open index.html in your browser
+
+## Dependancies
+
+ES2015/ES6 
---
+++ js/app.js
@@ -0,0 +1,106 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const sections = document.querySelectorAll('section');
+const navMenu = document.querySelector('#navbar__list');
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+function isInViewport(ele) {
+    let rectObject = ele.getBoundingClientRect();
+    return(rectObject.top >= 0 );
+}
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+function BuildNavMenu(section){
+    let sectionName = section.dataset.nav;
+    let sectionID = section.getAttribute('id');
+
+    let listElement = document.createElement('li');
+    // listElement.innerHTML = `<a data-id=${sectionID} class="menu__link">${sectionName}</a>`;
+    let anchorTag = document.createElement('a');
+    anchorTag.textContent = sectionName;
+    anchorTag.classList.add('menu__link');
+    anchorTag.dataset.id = sectionID;
+
+    addScrollToAction(anchorTag);
+    listElement.appendChild(anchorTag);
+    navMenu.appendChild(listElement);
+}
+
+
+// Add class 'active' to section when near top of viewport
+function addActive(element) {
+    document.addEventListener('scroll',function (event) { 
+        if(isInViewport(element)){
+            element.classList.add('active');
+        }
+     })
+ }
+
+// Scroll to anchor ID using scrollTO event
+function addScrollToAction(element){
+    element.addEventListener('click',function(event){
+        let dataset = event.target.dataset.id;
+        let section  = document.getElementById(dataset);
+        section.scrollIntoView(); 
+    });
+}
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+sections.forEach(function(section){
+    BuildNavMenu(section);
+});
+
+// Scroll to section on link click
+
+// Set sections as active
+// sections.forEach(function (section) { 
+//     addActive(section);
+// })
+document.addEventListener('scroll',function (event) { 
+    sections.forEach(function(element){
+        if(isInViewport(element)){
+            if(!element.classList.contains('active'))
+                element.classList.add('active');
+        }else{
+            element.classList.remove('active');
+        }
+    });    
+});
